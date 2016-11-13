6559b7a3ef54e3a29de2f4e2df1d8d88470775f3
Adhy <adhywiranatap@gmail.com>
2016-11-13 20:33:08 +0700

    refactored code - added routes for express

--- server/index.js
+++ server/index.js
@@ -1,111 +1,15 @@
-const express     = require('express');
-const bodyParser  = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
-const cors        = require('cors');
-// const objectId = require('mongodb').ObjectID;
+const express = require('express');
+
+const routes = require('./routes');
 
 const app   = express();
 const path  = require('path');
 
-let db;
-
 app.use(express.static('static'));
-app.use(cors());
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/app', express.static(path.join(__dirname, '../index.html')));
-
-app.get('/api/notes', (req, res) => {
-  db.collection('notes')
-    .find()
-    .sort({ title: 1 })
-    .toArray((err, docs) => {
-      if (err) {
-        console.log('error ', err)
-      } else {
-        res.json(docs);
-      }
-    });
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/api/note/', (req, res) => {
-  const newNoteData = req.body;
-  let newId = 0;
-
-  // Get Latest ID
-  db.collection('notes').find()
-    .sort({ id: -1 })
-    .limit(1)
-    .next((err, doc) => {
-      if (err) {
-        console.log('error ', err);
-      } else {
-        newId = doc.id;
-        newNoteData.id = newId + 1;
-        // Store newNoteData to DB
-        db.collection('notes').insertOne(newNoteData, (errInsert, result) => {
-          if (err) {
-            console.log('error ', err);
-          } else {
-            const newId = result.insertedId;
-            db.collection('notes').find({ _id: newId }).next((errInner, docInsert) => {
-              if (errInner) {
-                console.log('error ', errInner);
-              } else {
-                res.json(docInsert);
-              }
-            });
-          }
-        });
-      }
-    });
-});
-
-app.put('/api/note/', (req, res) => {
-  const updatedNoteData = req.body;
-
-  // Update updatedNoteData to DB
-  db.collection('notes')
-    .update({ id: updatedNoteData.id }, { $set: updatedNoteData }, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        db.collection('notes')
-          .find({ id: updatedNoteData.id })
-          .next((errInner, doc) => {
-            if (errInner) {
-              console.log('error ', errInner);
-            } else {
-              res.json(doc);
-            }
-          })
-      }
-    })
-});
-
-app.delete('/api/note/', (req, res) => {
-  const deletedNoteDataId = req.body.id;
-  db.collection('notes')
-    .remove({ id: deletedNoteDataId }, (err, result) => {
-      if (err) {
-        console.log('error ', err);
-      } else {
-        res.json(result);
-      }
-    });
-});
-
-// MongoDB Connector
-MongoClient.connect('mongodb://localhost/bukugajahdb', (err, dbConnection) => {
-  if (err) {
-    console.log('error!');
-  } else {
-    db = dbConnection;
-  }
-});
+app.use('/api', routes);
 
 const server = app.listen(5000, () => {
   const port = server.address().port;
---
+++ server/routes/index.js
@@ -0,0 +1,105 @@
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+const router = express.Router();
+
+router.use(cors());
+
+let db;
+
+// MongoDB Connector
+MongoClient.connect('mongodb://localhost/bukugajahdb', (err, dbConnection) => {
+  if (err) {
+    console.log('error!');
+  } else {
+    db = dbConnection;
+  }
+});
+
+router.get('/notes', (req, res) => {
+  db.collection('notes')
+    .find()
+    .sort({ title: 1 })
+    .toArray((err, docs) => {
+      if (err) {
+        console.log('error ', err)
+      } else {
+        res.json(docs);
+      }
+    });
+});
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/note/', (req, res) => {
+  const newNoteData = req.body;
+  let newId = 0;
+
+  // Get Latest ID
+  db.collection('notes').find()
+    .sort({ id: -1 })
+    .limit(1)
+    .next((err, doc) => {
+      if (err) {
+        console.log('error ', err);
+      } else {
+        newId = doc.id;
+        newNoteData.id = newId + 1;
+        // Store newNoteData to DB
+        db.collection('notes').insertOne(newNoteData, (errInsert, result) => {
+          if (err) {
+            console.log('error ', err);
+          } else {
+            const newId = result.insertedId;
+            db.collection('notes').find({ _id: newId }).next((errInner, docInsert) => {
+              if (errInner) {
+                console.log('error ', errInner);
+              } else {
+                res.json(docInsert);
+              }
+            });
+          }
+        });
+      }
+    });
+});
+
+
+router.put('/note/', (req, res) => {
+  const updatedNoteData = req.body;
+
+  // Update updatedNoteData to DB
+  db.collection('notes')
+    .update({ id: updatedNoteData.id }, { $set: updatedNoteData }, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        db.collection('notes')
+          .find({ id: updatedNoteData.id })
+          .next((errInner, doc) => {
+            if (errInner) {
+              console.log('error ', errInner);
+            } else {
+              res.json(doc);
+            }
+          })
+      }
+    })
+});
+
+router.delete('/note/', (req, res) => {
+  const deletedNoteDataId = req.body.id;
+  db.collection('notes')
+    .remove({ id: deletedNoteDataId }, (err, result) => {
+      if (err) {
+        console.log('error ', err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+module.exports = router;
