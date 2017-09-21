696283525a95e7ebca9d0e169ffec150efdcdfce
3hdeng <3hdeng@gmail.com>
2017-09-21 11:08:17 +0800

    app.use('/') not work as expected?

--- server/index.js
+++ server/index.js
@@ -4,17 +4,18 @@ const routes = require('./routes');
 
 const app   = express();
 const path  = require('path');
-const port= require('../src/actions/apis').PORT;
+const port= process.env.PORT || 3000; //require('../src/actions/apis').PORT;
 
 app.use(express.static('static'));
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use('/app', express.static(path.join(__dirname, '../index.html')));
 app.use('/api', routes);
+//app.get('*', function (request, response){ response.sendFile(path.resolve(__dirname, '../', 'index.html')) });
+app.use('/app', express.static(path.resolve(__dirname, '../index.html')));
 
-const server = app.listen(PORT, () => {
+const server = app.listen(port, () => {
   const port = server.address().port;
-  console.log('Bukugajah server running on ', port);
+  console.log('mynote-react server running on ', port);
 });
 
 module.exports = server;
