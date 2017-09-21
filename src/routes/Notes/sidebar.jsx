d5c2ad51c8578106f4c955a93cc51c07ca27261a
3hdeng <3hdeng@gmail.com>
2017-09-21 11:07:34 +0800

    store/index.js, routes/Notes/sidebar.jsx, notesData

--- src/routes/Notes/sidebar.jsx
+++ src/routes/Notes/sidebar.jsx
@@ -6,67 +6,74 @@ import { ButtonUI, IconUI } from '../../components/SemanticUI';
 import { connector }        from '../../store';
 
 const SidebarNoteItem = props => (
-  <Link to={`/editor/${props.id}`}>
-    <h2 className={css.noteLink}>{props.title}</h2>
-    <p className={css.textFaded}>{props.createdAt}</p>
-    <p>
-      {props.description}
-    </p>
-  </Link>
-);
+		<Link to={`/editor/${props.id}`}>
+		<h2 className={css.noteLink}>{props.title}</h2>
+		<p className={css.textFaded}>{props.createdAt}</p>
+		<p>
+		{props.description}
+		</p>
+		</Link>
+		);
 
 SidebarNoteItem.propTypes = {
-  id:           PropTypes.number,
-  title:        PropTypes.string,
-  createdAt:    PropTypes.string,
-  description:  PropTypes.string,
+id:           PropTypes.number,
+	      title:        PropTypes.string,
+	      createdAt:    PropTypes.string,
+	      description:  PropTypes.string,
 }
 
 class Sidebar extends Component {
 
-  componentDidMount() {
-    this.props.fetchNotes(); // from redux
-  }
+	componentDidMount() {
+		this.props.fetchNotes(); // from redux
+	}
 
-  render() {
-    const searchTerm = this.props.noteSearchTerm; // from redux
-    const notesData = this.props.notesData; // from redux
+	render() {
+		const searchTerm = this.props.noteSearchTerm; // from redux
+		const notesData = this.props.notesData; // from redux
 
-    const filteredNotesData = notesData.filter(
-      dataNote => (dataNote.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
-    );
+		const filteredNotesData = notesData.filter(
+				dataNote => {
+				if(dataNote && dataNote.title) 
+				return (dataNote.title.toLowerCase().indexOf(searchTerm.toLowerCase()) >=0 );
+				else return  false;
+				} 
+				);
 
-    return (
-      <div className={css.sidebar}>
-        <h2>Notes</h2>
-        <Link to="/editor">
-          <ButtonUI className={css.addButton}>
-            <IconUI name="add circle" />
-            Add a New Note
-          </ButtonUI>
-        </Link>
-        <p className={css.searchFilterContainer}>
-          <span>Filter: </span>
-          <span className={css.searchFilter}>{searchTerm}</span>
-        </p>
-        <div className={css.sidebarList}>
-          {
-            filteredNotesData.length < 1 ?
-              <h2 className={css.sidebarNotFound}>Not found</h2>
-              : filteredNotesData.map(
-                dataNote => <SidebarNoteItem key={dataNote.id} {...dataNote} />
-              )
-          }
-        </div>
-      </div>
-    );
-  }
+		return (
+				<div className={css.sidebar}>
+				<h2>Notes</h2>
+				<Link to="/editor">
+				<ButtonUI className={css.addButton}>
+				<IconUI name="add circle" />
+				Add a New Note
+				</ButtonUI>
+				</Link>
+				<p className={css.searchFilterContainer}>
+				<span>Filter: </span>
+				<span className={css.searchFilter}>{searchTerm}</span>
+				</p>
+				<div className={css.sidebarList}>
+				{
+				filteredNotesData.length < 1 ?
+				<h2 className={css.sidebarNotFound}>Not found</h2>
+				: filteredNotesData.map(
+					dataNote => <SidebarNoteItem key={dataNote.id} {...dataNote} />
+					)
+				}
+		</div>
+			</div>
+			);
+	}
 }
 
 Sidebar.propTypes = {
-  fetchNotes:     PropTypes.func,
-  noteSearchTerm: PropTypes.string,
-  notesData:      PropTypes.object,
+fetchNotes:     PropTypes.func,
+		noteSearchTerm: PropTypes.string,
+		notesData:      PropTypes.oneOfType([
+				PropTypes.array,
+				PropTypes.object
+				])
 }
 
 export default connector(Sidebar);
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import {
-  createStore,
-  applyMiddleware,
-  compose,
+	createStore,
+		applyMiddleware,
+		compose,
 } from 'redux';
 import { connect } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -11,121 +11,130 @@ import rootReducer from '../reducers';
 import * as ActionTypes from '../actions/constants';
 import * as API from '../actions/apis';
 import {
-  setNoteSearchTerm,
-  addNote,
-  updateNote,
-  deleteNote,
+	setNoteSearchTerm,
+		addNote,
+		updateNote,
+		deleteNotE,
+		fetchNotes,
+		fetchNoteDetails,
 } from '../actions';
 
 export const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk),
-  compose(
-    typeof window === 'object'
-    && typeof window.devToolsExtension !== 'undefined' ?
-      window.devToolsExtension() : func => func
-  )
-);
+		rootReducer,
+		applyMiddleware(thunk),
+		compose(
+			typeof window === 'object'
+			&& typeof window.devToolsExtension !== 'undefined' ?
+			window.devToolsExtension() : func => func
+		       )
+		);
 
 const mapStateToProps = state => ({
-  notesData: state.notesData,
-  noteSearchTerm: state.noteSearchTerm,
+notesData: state.notesData,
+noteSearchTerm: state.noteSearchTerm,
+noteDetailData: state.noteDetailData
 });
 
 const mapDispatchToProps = dispatch => ({
-  setNoteSearchTerm: noteSearchTerm => dispatch(setNoteSearchTerm(noteSearchTerm)),
-  addNote: (newNotesData) => {
-    dispatch(addNote(newNotesData));
-    fetch(API.STORE_NOTE, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newNotesData),
-    })
-      .then(response => response.json())
-      .then((json) => {
-        console.log('insert success!', json);
-      }).catch((ex) => {
-        console.log('parsing failed', ex);
-      });
-  },
-  updateNote: (updatedNotesData) => {
-    dispatch(updateNote(updatedNotesData));
-    fetch(API.UPDATE_NOTE, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedNotesData),
-    })
-      .then(response => response.json())
-      .then((json) => {
-        console.log('update success!', json);
-      }).catch((ex) => {
-        console.log('parsing failed', ex);
-      });
-  },
-  deleteNote: (deletedNoteDataId) => {
-    dispatch(deleteNote(deletedNoteDataId));
-    fetch(API.DELETE_NOTE, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id: deletedNoteDataId }),
-    })
-      .then(response => response.json())
-      .then((json) => {
-        console.log('delete success!', json);
-      }).catch((ex) => {
-        console.log('parsing failed', ex);
-      });
-  },
-  fetchNotes: () => {
-    dispatch({
-      type: ActionTypes.RECEIVE_NOTES,
-      text: 'fetch from server',
-      notesData: [],
-    });
-    fetch(API.FETCH_NOTES, {
-      method: 'GET',
-      mode: 'cors',
-    })
-      .then(response => response.json())
-      .then((json) => {
-        dispatch({
-          type: ActionTypes.RECEIVE_NOTES,
-          text: 'fetch from server',
-          notesData: json,
-        });
-      }).catch((ex) => {
-        console.log('parsing failed', ex);
-      });
-  },
-  fetchNoteDetails: (id) => {
-    dispatch({
-      type: ActionTypes.FETCH_NOTE_DETAILS,
-      text: 'fetch a note detail from server',
-      noteData: [],
-    });
-    fetch(API.FETCH_NOTE_DETAILS, {
-      method: 'GET',
-      mode: 'cors',
-    })
-      .then(response => response.json())
-      .then((json) => {
-        dispatch({
-          type: ActionTypes.FETCH_NOTE_DETAILS,
-          text: 'fetch a note detail from server',
-          noteData: json,
-        });
-      }).catch((ex) => {
-        console.log('parsing failed', ex);
-      });
-  },
+setNoteSearchTerm: noteSearchTerm => {
+dispatch(setNoteSearchTerm(noteSearchTerm));
+},
+addNote: (newNotesData) => {
+		 dispatch(addNote(newNotesData));
+		 fetch(API.STORE_NOTE, {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify(newNotesData),
+})
+.then(response => response.json())
+	.then((json) => {
+			console.log('insert success!', json);
+			}).catch((ex) => {
+				console.log('parsing failed', ex);
+				});
+},
+updateNote: (updatedNotesData) => {
+		    dispatch(updateNote(updatedNotesData));
+		    fetch(API.UPDATE_NOTE, {
+method: 'PUT',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify(updatedNotesData),
+})
+.then(response => response.json())
+	.then((json) => {
+			console.log('update success!', json);
+			}).catch((ex) => {
+				console.log('parsing failed', ex);
+				});
+},
+deleteNote: (deletedNoteDataId) => {
+		    dispatch(deleteNote(deletedNoteDataId));
+		    fetch(API.DELETE_NOTE, {
+method: 'DELETE',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify({ id: deletedNoteDataId }),
+})
+.then(response => response.json())
+	.then((json) => {
+			console.log('delete success!', json);
+			}).catch((ex) => {
+				console.log('parsing failed', ex);
+				});
+},
 
+
+fetchNotes: () => {
+		    dispatch({
+type: ActionTypes.RECEIVE_NOTES,
+text: 'fetch from server',
+notesData: [],
 });
+fetch(API.FETCH_NOTES, {
+method: 'GET',
+mode: 'cors',
+})
+.then(response => response.json())
+	.then(json => {
+			dispatch({
+type: ActionTypes.RECEIVE_NOTES,
+text: 'fetch from server',
+notesData: json,
+});
+			}).catch((ex) => {
+				console.log('parsing failed', ex);
+				});
+},
+
+
+fetchNoteDetails: (id) => {
+			  dispatch({
+type: ActionTypes.FETCH_NOTE_DETAILS,
+text: 'fetch a note detail from server',
+noteDetailData: {},
+});
+fetch(API.FETCH_NOTE_DETAILS+ id.toString(), {
+method: 'GET',
+mode: 'cors',
+})
+.then(response => response.json())
+	.then((json) => {
+			dispatch({
+type: ActionTypes.FETCH_NOTE_DETAILS,
+text: 'fetch a note detail from server',
+noteDetailData: json,
+});
+			}).catch((ex) => {
+				console.log('parsing failed', ex);
+				});
+},
+
+	});
 
 
 export const connector = connect(mapStateToProps, mapDispatchToProps);
